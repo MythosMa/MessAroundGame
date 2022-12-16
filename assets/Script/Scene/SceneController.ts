56d46688ff4c4e2cc86eec44aa578bf658ba913d
MythosMa <mythosmayuan@gmail.com>
2022-12-16 18:42:38 +0800

    update

--- assets/Script/Scene/SceneController.ts
+++ assets/Script/Scene/SceneController.ts
@@ -1,4 +1,16 @@
-import { _decorator, Component, Prefab, Node, NodePool, instantiate, Vec2, UITransform, math, Camera, Vec3 } from "cc";
+import {
+  _decorator,
+  Component,
+  Prefab,
+  Node,
+  NodePool,
+  instantiate,
+  Vec2,
+  UITransform,
+  math,
+  Camera,
+  Vec3,
+} from "cc";
 import { changePosition } from "../utils/nodeScriptTools";
 const { ccclass, property } = _decorator;
 
@@ -17,12 +29,18 @@ export class SceneController extends Component {
   map = null;
 
   @property(Node)
-  canvas = null
+  canvas = null;
 
+  @property(Number)
+  cameraStartX = 0;
+
+  @property(Number)
+  camreaEndX = 0;
 
   bulletPool: NodePool = null;
   mapSize: math.Size = null;
   canvasSize: math.Size = null;
+  cameraStartMoveDirection: number = 1;
 
   onLoad() {
     this.bulletPool = new NodePool();
@@ -42,17 +60,32 @@ export class SceneController extends Component {
   }
 
   update(deltaTime: number) {
-    // this.changeCameraPosition();
+    this.changeCameraPosition();
     // console.log("update====================");
-    // console.log(this.camera.position);
+    console.log(this.camera.position);
     // console.log(this.camera);
     // console.log("update====================");
-    this.cameraMove()
+    // this.cameraMove()
   }
 
   changeCameraPosition() {
     let currentPosition = this.camera.position;
-    changePosition(currentPosition, 10, "x");
+    changePosition(currentPosition, 10 * this.cameraStartMoveDirection, "x");
+
+    if (
+      this.cameraStartMoveDirection > 0 &&
+      currentPosition.x > this.camreaEndX
+    ) {
+      // this.cameraStartMoveDirection = -1;
+      currentPosition.x = this.camreaEndX;
+    } else if (
+      this.cameraStartMoveDirection < 0 &&
+      currentPosition.x < this.cameraStartX
+    ) {
+      this.cameraStartMoveDirection = 1;
+      currentPosition.x = this.cameraStartX;
+    }
+
     this.camera.setPosition(currentPosition);
   }
 
@@ -70,8 +103,20 @@ export class SceneController extends Component {
     this.bulletPool.put(bullet);
   }
 
-  cameraMove(){
-    const playerInScreenPosition = this.camera.getComponent(Camera).worldToScreen(new Vec3(this.player.getPosition().x, this.player.getPosition().y, 0))
-    const cameraPosition = this.camera.position
+  cameraMove() {
+    const playerInScreenPosition = this.camera
+      .getComponent(Camera)
+      .worldToScreen(
+        new Vec3(this.player.getPosition().x, this.player.getPosition().y, 0)
+      );
+    const cameraPosition = this.camera.position;
+    // canvas的锚点是 0.5 0.5，所以在计算 角色在屏幕中的坐标时，为了精确需要补差值
+    const playerInScreenPercent =
+      (playerInScreenPosition.x + this.canvasSize.width / 2) /
+      this.canvasSize.width;
+    console.log("playerInScreenPercent================");
+    console.log(playerInScreenPosition);
+    console.log(this.canvasSize.width);
+    console.log("playerInScreenPercent================");
   }
 }
